f7b9cdf47d290a278bce52fdd92ffa5e31d061d6
dadar870218 <dadar870218@gmail.com>
2024-03-06 19:35:43 +0100

    fixed error in homepost

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,5 +1,25 @@
 const HomePosts = () => {
-  return <div>HomePosts</div>;
+  return (
+    <div className="w-full flex mt-8 space-x-4">
+      {/*left*/}
+      <div className="w-[35%] h-[200px] flex justify-center items-center">
+        <img
+          src="https://www.disneyholidays.com/walt-disney-world/images/tickets/desktop/young-girl-at-blizzard-beach-water-park.jpg"
+          alt=""
+          className=""
+        />
+      </div>
+      {/*right*/}
+      <div className="flex flex-col w-[65%]">
+        <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
+          disney all the way
+        </h1>
+
+        <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4"></div>
+        <p>@danieldev</p>
+      </div>
+    </div>
+  );
 };
 
 export default HomePosts;
