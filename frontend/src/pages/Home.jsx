e8ef3f4f0bc3bd59677c31f51d51871fc1006689
dadar870218 <dadar870218@gmail.com>
2024-03-06 21:07:54 +0100

    added classes and paraghaphs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import HomePosts from "../components/HomePosts";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-8 md:px-[200px]">
+      <HomePosts />
+      <HomePosts />
+      <HomePosts />
+      <HomePosts />
+      <HomePosts />
+    </div>
+  );
 };
 
 export default Home;
