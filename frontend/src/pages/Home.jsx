92e0c478d2a813e3e8f77d60fc0282ae2d7c4813
dadar870218 <dadar870218@gmail.com>
2024-03-06 23:13:01 +0100

    added Navbar route

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import HomePosts from "../components/HomePosts";
-
+import Navbar from "../components/Navbar";
 const Home = () => {
   return (
-    <div className="px-8 md:px-[200px]">
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-    </div>
+    <>
+      <Navbar />
+      <div className="px-8 md:px-[200px]">
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+      </div>
+      <footer />
+    </>
   );
 };
 
