0d1fd50d461629fc43caf7da2f613b8b55138989
dadar870218 <dadar870218@gmail.com>
2024-03-10 12:25:41 +0100

    added routes to editpost and profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PostDetails from "./pages/PostDetails";
 import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -13,8 +15,9 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/write" element={<CreatePost />} />
-
         <Route exact path="/posts/post/:id" element={<PostDetails />} />
+        <Route exact path="/edit/:id" element={<EditPost />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
       </Routes>
     </div>
   );
