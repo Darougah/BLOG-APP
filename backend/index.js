7065885cca7426c30380dbebc1887e23b66b553b
dadar870218 <dadar870218@gmail.com>
2024-03-12 11:42:38 +0100

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-//database
+const authRoute = require("./routes/auth");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database is connected successfully!");
   } catch (err) {
     console.log(err);
@@ -13,8 +15,11 @@ const connectDB = async () => {
 };
 
 //middlewares
+dotenv.config();
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   connectDB();
-  console.log("app is running on port 5000");
+  console.log("app is running on port " + process.env.PORT);
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,19 +5,18 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 //Register
-router.post(
-  "/register",
-  async(req, (res, res) => {
-    try {
-      const { username, email, password } = req.body;
-      const NewUser = new User({ username, email, password });
-      const SavedUser=await NewUser.save()
-      res.status(200).json(SavedUser)
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  })
-);
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //Login
 
 //Logout
+
+module.exports = router;
