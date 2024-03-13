ad0e3faba98b3ca1cfc924a95d3955f0461a8d90
dadar870218 <dadar870218@gmail.com>
2024-03-13 12:22:27 +0100

    updated with verifytoken

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const commentRoute = require("./routes/comments");
+const cookieParser = require("cookie-parser");
 
 const connectDB = async () => {
   try {
@@ -19,6 +20,7 @@ const connectDB = async () => {
 //middlewares
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
