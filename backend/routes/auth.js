9809ead9a585ca5c53553a358763bfaf99d762ac
dadar870218 <dadar870218@gmail.com>
2024-03-13 09:44:13 +0100

    login route auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,7 +18,25 @@ router.post("/register", async (req, res) => {
   }
 });
 //Login
-
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json("User not found!");
+    }
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) {
+      return res.status(401).json("Wrong credentials!");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      expiresIn: "3d",
+    });
+    const { password, ...info } = user._doc;
+    res.cookie("token", token).status(200).json(info);
+  } catch (err) {
+    res.status(200).json(err);
+  }
+});
 //Logout
 
 module.exports = router;
