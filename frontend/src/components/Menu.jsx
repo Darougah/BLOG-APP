72527f5ff7a235350dc19fad4613bbc35619303c
dadar870218 <dadar870218@gmail.com>
2024-03-14 16:07:03 +0100

    add useContext

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 const Menu = () => {
-  const user = false;
+  const { user } = useContext(UserContext);
   return (
     <div className="bg-black w-[200px] flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
       {!user && (
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import { FaBars } from "react-icons/fa6";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Menu from "./Menu";
+import { UserContext } from "../context/UserContext";
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
   const showMenu = () => {
     setMenu(!menu);
   };
-  const user = false;
+  const { user } = useContext(UserContext);
 
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
