b6efad249cdd3fcc690bcffc9a4ccb194a4a5b89
dadar870218 <dadar870218@gmail.com>
2024-03-14 16:31:28 +0100

    refetch user so it doesnt logout automaticly after refreshing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-//Register
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -17,27 +17,33 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
-//Login
+
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
+
     if (!user) {
       return res.status(404).json("User not found!");
     }
     const match = await bcrypt.compare(req.body.password, user.password);
+
     if (!match) {
       return res.status(401).json("Wrong credentials!");
     }
-    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
-      expiresIn: "3d",
-    });
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, email: user.email },
+      process.env.SECRET,
+      { expiresIn: "3d" }
+    );
     const { password, ...info } = user._doc;
     res.cookie("token", token).status(200).json(info);
   } catch (err) {
-    res.status(200).json(err);
+    res.status(500).json(err);
   }
 });
-//Logout
+
+//LOGOUT
 router.get("/logout", async (req, res) => {
   try {
     res
@@ -48,4 +54,16 @@ router.get("/logout", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//REFETCH USER
+router.get("/refetch", (req, res) => {
+  const token = req.cookies.token;
+  jwt.verify(token, process.env.SECRET, {}, async (err, data) => {
+    if (err) {
+      return res.status(404).json(err);
+    }
+    res.status(200).json(data);
+  });
+});
+
 module.exports = router;
