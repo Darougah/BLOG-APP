35abd11018dac71d3b8d137236141b8713ff9357
dadar870218 <dadar870218@gmail.com>
2024-03-14 13:37:15 +0100

    added cors cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const commentRoute = require("./routes/comments");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const connectDB = async () => {
   try {
