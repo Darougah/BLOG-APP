79c4d430cfc0434627122303b9eb3ef27e5c3f1e
dadar870218 <dadar870218@gmail.com>
2024-03-15 15:57:41 +0100

    post

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,34 +1,24 @@
-const HomePosts = () => {
+const HomePosts = ({ post }) => {
   return (
     <div className="w-full flex mt-8 space-x-4">
       {/* left */}
       <div className="w-[35%] h-[200px] flex justify-center items-center">
-        <img
-          src="https://miro.medium.com/v2/resize:fit:1400/1*Hc1p9ZDHT5NZnuUdEToj4w.png"
-          alt=""
-          className="h-full w-full object-cover"
-        />
+        <img src={post.photo} alt="" className="h-full w-full object-cover" />
       </div>
       {/* right */}
       <div className="flex flex-col w-[65%]">
         <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
-          10 uses of artificial intelligence in daily life
+          {post.title}
         </h1>
         <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
-          <p>@danield</p>
+          <p>@{post.username}</p>
           <div className="flex space-x-2 text-sm">
-            <p>2024-03-06</p>
-            <p>20:45</p>
+            <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
           </div>
         </div>
         <p className="text-sm md:text-lg">
-          {" "}
-          If we think about a decade back, Artificial Intelligence (AI) seemed
-          like science fiction. Fast-forward to today and AI has become integral
-          to our everyday existence. From voice-activated assistants
-          anticipating our needs to social media feeds seemingly reading our
-          minds, AI is quietly revolutionizing our world. We have become so used
-          to these being in ours
+          {post.desc.slice(0, 200) + " ...Read more"}
         </p>
       </div>
     </div>
