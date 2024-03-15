098165079b1010575b41d706b90300db5b9a1391
dadar870218 <dadar870218@gmail.com>
2024-03-15 17:19:47 +0100

    added loader

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,21 +5,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { URL } from "../url";
 import { useLocation } from "react-router-dom";
+import Loader from '../components/Loader'
 
 const Home = () => {
   const { search } = useLocation();
   const [posts, setPosts] = useState([]);
   const [noResults, setNoResults] = useState(false);
+  const [loader,setLoader]=useState(false)
 
   const fetchPost = async () => {
-    try {
-      const res = await axios.get(URL + "/api/posts/" + search);
-      setPosts(res.data);
-      setNoResults(res.data.length === 0);
-    } catch (err) {
-      console.log(err);
+    setLoader(true)
+    try{
+      const res=await axios.get(URL+"/api/posts/"+search)
+      // console.log(res.data)
+      setPosts(res.data)
+      if(res.data.length===0){
+        setNoResults(true)
+      }
+      else{
+        setNoResults(false)
+      }
+      setLoader(false)
+      
     }
-  };
+    catch(err){
+      console.log(err)
+      setLoader(true)
+    }
+  }
 
   useEffect(() => {
     fetchPost();
@@ -29,13 +42,10 @@ const Home = () => {
     <>
       <Navbar />
       <div className="px-8 md:px-[200px] min-h-[80vh]">
-        {noResults ? (
-          <h3 className="text-center font-bold mt-16">No posts available</h3>
-        ) : (
-          posts.map((post) => (
+        {loader?<div className="h-[40vh] flex justify-center items-center"><Loader/></div>:!noResults?posts.map((post) => (
             <HomePosts key={post._id} post={post} />
-          ))
-        )}
+            )): <h3 className="text-center font-bold mt-16">No posts available</h3>}
+
       </div>
       <Footer />
     </>
