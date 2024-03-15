e393e4de936adf0683fbd4ab8c58e83679bf98dd
dadar870218 <dadar870218@gmail.com>
2024-03-15 15:58:04 +0100

    added no result when search

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,18 +7,15 @@ import { URL } from "../url";
 import { useLocation } from "react-router-dom";
 
 const Home = () => {
-
-const {search}=useLocation()
-// console.log(search)
-
-
+  const { search } = useLocation();
   const [posts, setPosts] = useState([]);
+  const [noResults, setNoResults] = useState(false);
 
   const fetchPost = async () => {
     try {
-      const res = await axios.get(URL + "/api/posts/"+search);
-      // console.log(res.data);
+      const res = await axios.get(URL + "/api/posts/" + search);
       setPosts(res.data);
+      setNoResults(res.data.length === 0);
     } catch (err) {
       console.log(err);
     }
@@ -31,10 +28,14 @@ const {search}=useLocation()
   return (
     <>
       <Navbar />
-      <div className="px-8 md:px-[200px] min-h[80vh]">
-        {posts.map((post) => (
-          <HomePosts key={post._id} post={post} />
-        ))}
+      <div className="px-8 md:px-[200px] min-h-[80vh]">
+        {noResults ? (
+          <h3 className="text-center font-bold mt-16">No posts available</h3>
+        ) : (
+          posts.map((post) => (
+            <HomePosts key={post._id} post={post} />
+          ))
+        )}
       </div>
       <Footer />
     </>
