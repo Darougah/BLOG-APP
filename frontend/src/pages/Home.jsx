b040f894c0fbd854077746dcb7d03b2ebe5142d5
dadar870218 <dadar870218@gmail.com>
2024-03-15 15:45:27 +0100

    add search for post

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,40 @@
 import HomePosts from "../components/HomePosts";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { URL } from "../url";
+import { useLocation } from "react-router-dom";
+
 const Home = () => {
+
+const {search}=useLocation()
+// console.log(search)
+
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPost = async () => {
+    try {
+      const res = await axios.get(URL + "/api/posts/"+search);
+      // console.log(res.data);
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, [search]);
+
   return (
     <>
       <Navbar />
-      <div className="px-8 md:px-[200px]">
-        <HomePosts />
-        <HomePosts />
-        <HomePosts />
-        <HomePosts />
-        <HomePosts />
+      <div className="px-8 md:px-[200px] min-h[80vh]">
+        {posts.map((post) => (
+          <HomePosts key={post._id} post={post} />
+        ))}
       </div>
       <Footer />
     </>
