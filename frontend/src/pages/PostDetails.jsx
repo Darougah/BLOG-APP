240c68cf525bd64de1a38d13b662fb04605dc40c
dadar870218 <dadar870218@gmail.com>
2024-03-24 12:21:20 +0100

    fix so only user of post can post details , delete

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -3,71 +3,98 @@ import Footer from "./../components/Footer.jsx";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import Comment from "../components/Comment.jsx";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { URL } from "../url.js";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext.jsx";
+import Loader from './../components/Loader';
 
 const PostDetails = () => {
+  const postId = useParams().id;
+  const [post, setPost] = useState({});
+  const {user} = useContext(UserContext);
+  const [loader, setLoader] = useState(false);
+
+  const fetchPost = async () => {
+    setLoader(true);
+    try {
+      const res = await axios.get(URL + "/api/posts/" + postId);
+      setPost(res.data);
+      setLoader(false);
+    } catch (err) {
+      console.log(err);
+      setLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, [postId]);
+
   return (
     <div>
       <Navbar />
-      <div className="px-8 md:px-[200px] mt-8">
-        <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-bold text-black md:text-3xl">
-            10 uses of artificial intelligence in daily life
-          </h1>
-          <div className="flex items-center justify-center space-x-2">
-            <BiEdit />
-            <MdDelete />
+      {loader ? (
+        <div className="h-[80vh] flex justify-center items-center w-full">
+          <Loader />
+        </div>
+      ) : (
+        <div className="px-8 md:px-[200px] mt-8">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl font-bold text-black md:text-3xl">
+              {post.title}
+            </h1>
+            {user?._id === post?.userId && (
+              <div className="flex items-center justify-center space-x-2">
+                <p>
+                  <BiEdit />
+                </p>
+                <p>
+                  <MdDelete />
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex items-center justify-between mt-2 md:mt-4">
+            <p>@{post.username}</p>
+            <div className="flex space-x-2 text-sm">
+              <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+              <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
+            </div>
+          </div>
+          <img src={post.photo} className="w-full mx-auto mt-8" alt="" />
+          <p className="mx-auto mt-8">{post.desc}</p>
+          <div className="flex items-center mt-8 space-x-4 font-semibold">
+            <p>categories</p>
+            <div className="flex justify-center items-center space-x-2">
+              {post.categories?.map((c, i) => (
+                <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">
+                  {c}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col mt-4">
+            <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
+            {/* write a comment */}
+            <Comment />
+            <Comment />
+            {/* write a comment */}
+          </div>
+          {/* Write a comment */}
+          <div className="w-full flex flex-col mt-4 md:flex-row">
+            <input
+              type="text"
+              placeholder="write a comment"
+              className="md:w-[80%] outline-none px-4 mt-4 md:mt-0"
+            ></input>
+            <button className="bg-black text-sm text-white px-4 py-2 md:w[10%] mt-4 md:mt-0">
+              Add a comment
+            </button>
           </div>
         </div>
-        <div className="flex items-center justify-between mt-2 md:mt-4">
-          <p>@danield</p>
-          <div className="flex space-x-2 text-sm">
-            <p>2024-03-06</p>
-            <p>20:45</p>
-          </div>
-        </div>
-        <img
-          src="https://miro.medium.com/v2/resize:fit:1400/1*Hc1p9ZDHT5NZnuUdEToj4w.png"
-          className="w-full mx-auto mt-8"
-          alt=""
-        />
-        <p className="mx-auto mt-8">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book. It has survived not only five centuries,
-          but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem Ipsum passages, and more recently
-          with desktop publishing software like Aldus PageMaker including
-          versions of Lorem Ipsum.
-        </p>
-        <div className="flex items-center mt-8 space-x-4 font-semibold">
-          <p>categories</p>
-          <div className="flex justify-center items-center space-x-2">
-            <div className="bg-gray-300 rounded-lg px-3 py-1">Tech</div>
-            <div className="bg-gray-300 rounded-lg px-3 py-1">Ai</div>
-          </div>
-        </div>
-        <div className="flex flex-col mt-4">
-          <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
-          {/* write a comment */}
-          <Comment />
-          <Comment />
-
-          {/* write a comment */}
-        </div>
-        {/* Write a comment */}
-        <div className="w-full flex flex-col mt-4 md:flex-row">
-          <input
-            type="text"
-            placeholder="write a comment"
-            className="md:w-[80%] outline-none px-4 mt-4 md:mt-0"
-          ></input>
-          <button className="bg-black text-sm text-white px-4 py-2 md:w[10%] mt-4 md:mt-0">
-            Add a comment
-          </button>
-        </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
