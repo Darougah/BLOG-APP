eba79646114ee6707c2eac9af1b7fe7416a0c075
dadar870218 <dadar870218@gmail.com>
2024-03-26 19:51:41 +0100

    navigate

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -2,10 +2,13 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import axios from "axios";
 import { URL } from "../url";
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
   const { user } = useContext(UserContext);
   const { setUser } = useContext(UserContext);
+
+const navigate = useNavigate()
 
   const handleLogout = async () => {
     try {
@@ -13,6 +16,7 @@ const Menu = () => {
         withCredentials: true,
       });
       setUser(null);
+      navigate("/login")
     } catch (err) {
       console.log(err);
     }
