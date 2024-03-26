d72ff735d9817b8ce7e35e9081f30bce47887ddb
dadar870218 <dadar870218@gmail.com>
2024-03-26 18:02:02 +0100

    changed user to post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -20,12 +20,12 @@ router.post("/create", verifyToken, async (req, res) => {
 //UPDATE
 router.put("/:id", verifyToken, async (req, res) => {
   try {
-    const updatedUser = await Post.findByIdAndUpdate(
+    const updatedPost= await Post.findByIdAndUpdate(
       req.params.id,
       { $set: req.body },
       { new: true }
     );
-    res.status(200).json(updatedUser);
+    res.status(200).json(updatedPost);
   } catch (err) {
     res.status(500).json(err);
   }
