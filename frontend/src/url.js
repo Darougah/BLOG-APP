0bd77381a85a383755a6f53ad911dbbd31081a64
dadar870218 <dadar870218@gmail.com>
2024-04-25 12:46:55 +0200

    test

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:5000";
+export const URL = "https://danielblogg.onrender.com";
 
-export const IF="http://localhost:5000/images/"
+export const IF="https://danielblogg.onrender.com/images/"
