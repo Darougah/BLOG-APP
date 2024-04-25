a4d38c9d3a5cd06b2e7b72de856962f3beaa4061
dadar870218 <dadar870218@gmail.com>
2024-04-25 13:02:11 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,11 @@ const connectDB = async () => {
 // Middleware setup
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// Allow multiple origins to make requests
+const allowedOrigins = ["http://localhost:5173", "https://daniel-blog-app.vercel.app/"];
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
