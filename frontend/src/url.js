3ad87f14fccc9e9703176e98fc7c11fa9c14998c
dadar870218 <dadar870218@gmail.com>
2024-04-25 22:56:10 +0200

    added the localhost

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,9 @@
 export const URL = "https://blogapp-a526.onrender.com";
 
 export const IF="https://blogapp-a526.onrender.com/images/"
+
+
+
+// export const URL = "http://localhost:5000";
+
+// export const IF="http://localhost:5000/images/"
