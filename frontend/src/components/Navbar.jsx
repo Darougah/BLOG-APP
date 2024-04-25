1883328acf2b997817723d73eda411414bbdd847
dadar870218 <dadar870218@gmail.com>
2024-04-25 11:18:41 +0200

    changed start to npm start and changed blog name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
       <h1 className="text-lg md:text-xl font-extrabold">
-        <Link to="/">Blog Market</Link>
+        <Link to="/">Daniel Blog</Link>
       </h1>
       {path === "/" && (
         <div className="flex justify-center items-center space-x-0">
