ffc8889405b692dc7ae9d913c6a7fc4b1e1d1ca0
dadar870218 <dadar870218@gmail.com>
2024-05-02 19:31:40 +0200

    added swearwords to editpost also

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -7,82 +7,97 @@ import axios from "axios";
 import { URL } from "../url";
 import { useNavigate, useParams } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+
+// List of swear words to censor
+const swearWords = ["fan", "jävla", "helvete" , "skit"];
+
+// Function to censor swear words
+const censorSwearWords = (text) => {
+  let censoredText = text;
+  swearWords.forEach((word) => {
+    const regex = new RegExp("\\b" + word + "\\b", "gi");
+    censoredText = censoredText.replace(regex, "*".repeat(word.length));
+  });
+  return censoredText;
+};
+
 const EditPost = () => {
-  const postId= useParams().id
-  const {user}=useContext(UserContext)
-  const navigate=useNavigate()
-  
-const [title, setTitle]=useState("")
-const [desc,setDesc] = useState("")
-const [file,setFile] = useState(null)
+  const postId = useParams().id;
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
   const [cat, setCat] = useState("");
   const [cats, setCats] = useState([]);
 
-  const fetchPost=async()=>{
-    try{
-      const res=await axios.get(URL+"/api/posts/"+postId)
-      setTitle(res.data.title)
-      setDesc(res.data.desc)
-      setFile(res.data.photo)
-      setCats(res.data.categories)
+  const fetchPost = async () => {
+    try {
+      const res = await axios.get(URL + "/api/posts/" + postId);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+      setFile(res.data.photo);
+      setCats(res.data.categories);
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-      console.log(err)
-    }
-  }
-  const handleUpdate=async (e)=>{
-    e.preventDefault()
-    const post={
-      title,
-      desc,
-      username:user.username,
-      userId:user._id,
-      categories:cats
-    }
-    if(file){
-      const data=new FormData()
-      const filename=Date.now()+file.name
-      data.append("img",filename)
-      data.append("file",file)
-      post.photo=filename
-      // console.log(data)
-      //img upload
-      try{
-        // eslint-disable-next-line no-unused-vars
-        const imgUpload=await axios.post(URL+"/api/upload",data)
-        // console.log(imgUpload.data)
-      }
-      catch(err){
-        console.log(err)
-      }
-    }
-    //post upload
+  };
 
-    try{
-      const res=await axios.put(URL+"/api/posts/"+postId,post,{withCredentials:true})
-      navigate("/posts/post/"+res.data._id)
-      // console.log(res.data)
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const censoredTitle = censorSwearWords(title);
+    const censoredDesc = censorSwearWords(desc);
+    const censoredCats = cats.map((cat) => censorSwearWords(cat));
 
+    const post = {
+      title: censoredTitle,
+      desc: censoredDesc,
+      username: user.username,
+      userId: user._id,
+      categories: censoredCats
+    };
+
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("img", filename);
+      data.append("file", file);
+      post.photo = filename;
+
+      try {
+        await axios.post(URL + "/api/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
     }
-    catch(err){
-      console.log(err)
+
+    try {
+      const res = await axios.put(URL + "/api/posts/" + postId, post, { withCredentials: true });
+      navigate("/posts/post/" + res.data._id);
+    } catch (err) {
+      console.log(err);
     }
-  }
-useEffect(()=>{
-  fetchPost()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-},[postId])
+  };
+
+  useEffect(() => {
+    fetchPost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [postId]);
+
   const deleteCategory = (i) => {
     let updatedCats = [...cats];
-    updatedCats.splice(i, 1); // splice takes the index and the number of items to remove
+    updatedCats.splice(i, 1);
     setCats(updatedCats);
   };
+
   const addCategory = () => {
     let updatedCats = [...cats];
     updatedCats.push(cat);
     setCat("");
     setCats(updatedCats);
   };
+
   return (
     <div>
       <Navbar />
@@ -90,12 +105,13 @@ useEffect(()=>{
         <h1 className="font-bold md:text-2xl text-xl">Update a post</h1>
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
           <input
-          onChange={(e)=>setTitle(e.target.value)} value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
             type="text"
             placeholder="Enter post title"
             className="px-4-py-2 outline-none"
           />
-          <input onChange={(e)=>setFile(e.target.files[0])} type="file" className="px-4" />
+          <input onChange={(e) => setFile(e.target.files[0])} type="file" className="px-4" />
           <div className="flex flex-col">
             <div className="flex items-center space-x-4 md:space-x-8">
               <input
@@ -132,13 +148,17 @@ useEffect(()=>{
             </div>
           </div>
           <textarea
-          onChange={(e)=>setDesc(e.target.value)} value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            value={desc}
             rows={15}
             cols={30}
             className="px-4 py-2 outline-none"
             placeholder="Enter post description"
           />
-          <button onClick={handleUpdate} className="bg-blue-500 w-full md:w[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg">
+          <button
+            onClick={handleUpdate}
+            className="bg-blue-500 w-full md:w[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg"
+          >
             Update
           </button>
         </form>
@@ -147,4 +167,5 @@ useEffect(()=>{
     </div>
   );
 };
+
 export default EditPost;
